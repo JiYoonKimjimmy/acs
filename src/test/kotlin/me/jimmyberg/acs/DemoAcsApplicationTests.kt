0174e0c52195df425c5379ddd117bbaf82383bb7
jimmyberg <jimmyberg.kim@gmail.com>
2023-01-03 20:56:58 +0900

    Initial commit

---
+++ src/test/kotlin/me/jimmyberg/acs/DemoAcsApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jimmyberg.acs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoAcsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
