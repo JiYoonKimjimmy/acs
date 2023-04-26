db2e59a24bb0456ff84187c39f6ccad745be1ad8
jimmyberg <jimmyberg.kim@gmail.com>
2023-04-26 20:45:04 +0900

    주소 연계 API 테스트 코드 수정
    - `content` 별 daily 다운로드 처리할 수 있도록 함수 수정

--- src/test/kotlin/me/jimmyberg/acs/core/ads/ADSClientTest.kt
+++ src/test/kotlin/me/jimmyberg/acs/core/ads/ADSClientTest.kt
@@ -11,23 +11,39 @@ import java.time.format.DateTimeFormatter
  */
 class ADSClientTest {
 
+    /**
+     * [자료 요청 구분]
+     * - 100001: 도로명주소(한글)
+     * - 100005: 도로명상세
+     * - 200002: 기초번호
+     */
+
     @Test
-    fun `당일 도로명주소(한글) '100001' 파일 다운로드 테스트`() {
-        daily(today = today())
+    fun `100001 daily 다운로드 테스트`() {
+        daily(content = "100001", today = today())
+    }
+
+    @Test
+    fun `100005 daily 다운로드 테스트`() {
+        daily(content = "100005", today = today())
+    }
+
+    @Test
+    fun `200002 daily 다운로드 테스트`() {
+        daily(content = "200002", today = today())
     }
 
     private fun today(): String {
         return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
     }
 
-    private fun daily(today: String) {
+    private fun daily(content: String, today: String) {
         val key      = "U01TX0FVVEgyMDIzMDQxNTEzNDU0NzExMzY4OTI=" // 승인키
         val dateType = "D"        // 날짜 구분
-        val content  = "100001"   // 자료 요청 구분 (100001: 도로명주소(한글), 009000: )
         val retry    = "Y"        // 재반영 요청 여부
 
         val ads = ADSReceiver()
-        ads.setFilePath("files/ADS_100001")
+        ads.setFilePath("files/ADS_$content")
         ads.setCreateDateDirectory(ADSUtils.YYYYMMDD)
 
         val receiveAddr = ads.receiveAddr(key, dateType, content, retry, today, today)
