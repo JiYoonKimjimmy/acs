db78b8a2d58fd67aca39160fa6866326456165a2
jimmyberg <jimmyberg.kim@gmail.com>
2023-05-20 11:27:42 +0900

    mod: FileManagementUtilTest 테스트코드 방식 수정
    - `unzip()` 테스트코드 분리

--- src/test/kotlin/me/jimmyberg/acs/core/ads/util/FileManagementUtilTest.kt
+++ src/test/kotlin/me/jimmyberg/acs/core/ads/util/FileManagementUtilTest.kt
@@ -5,13 +5,37 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 import java.nio.charset.Charset
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.util.zip.ZipFile
 
 class FileManagementUtilTest {
 
     @Test
     fun `압축 파일 Unzip 테스트`() {
+        val basePath = System.getProperty("user.dir")
+        val targetPath = "$basePath/files/ADS_100001/20230519"
+        val sourcePath = "$targetPath/AlterD.JUSUKR.20230519.ZIP"
+
+        val util = FileManagementUtil()
+
+        ZipFile(sourcePath).use { zip ->
+            zip.entries().asSequence().forEach { entry ->
+                println("entry : $entry")
+                val fileName = entry.name
+                val newFile = File("$targetPath${File.separator}$fileName")
+                // directory 생성
+                File(newFile.parent).mkdirs()
+                // unzip file 생성
+                zip.getInputStream(entry).use { input ->
+                    File(targetPath, fileName).outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `unzip() 함수 테스트`() {
         val filePath = "/files/ADS_100001/20230519"
         val fileName = "AlterD.JUSUKR.20230519.ZIP"
         val sourcePath = "$filePath/$fileName"
