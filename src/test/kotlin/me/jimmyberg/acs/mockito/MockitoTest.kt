8e9e5d178b96c4abd59faec2b1810f004694ad35
jimmyberg <jimmyberg.kim@gmail.com>
2023-07-05 20:51:59 +0900

    add: MockitoTest.kt 추가
    - `Mockito` 기본 동작 확인을 위한 테스트 코드 추가

--- src/test/kotlin/me/jimmyberg/acs/mockito/MockitoTest.kt
+++ src/test/kotlin/me/jimmyberg/acs/mockito/MockitoTest.kt
@@ -1,15 +1,21 @@
 package me.jimmyberg.acs.mockito
 
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 class MockitoTest {
 
     @DisplayName("Mockito 테스트코드 기본 동작을 확인한다.")
     @Test
     fun sampleTest() {
+        val mockedList = mock(List::class.java)
 
+        `when`(mockedList.size).thenReturn(5)
+
+        assertEquals(5, mockedList.size)
     }
 
 }
