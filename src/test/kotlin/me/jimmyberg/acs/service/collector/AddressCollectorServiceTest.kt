e1f0309c244af9ef9dab36038e85b46c32660c06
jimmyberg <jimmyberg.kim@gmail.com>
2023-07-19 20:46:04 +0900

    fix: AddressCollectorServiceTest 테스트 클래스 수정
    - `@Mock` 활용하여 `AddressCollectorService` 클래스 Mocking test 진행

--- src/test/kotlin/me/jimmyberg/acs/service/collector/AddressCollectorServiceTest.kt
+++ src/test/kotlin/me/jimmyberg/acs/service/collector/AddressCollectorServiceTest.kt
@@ -2,27 +2,47 @@ package me.jimmyberg.acs.service.collector
 
 import me.jimmyberg.acs.support.enumerate.ADSContent
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
 
-class AddressCollectorServiceTest {
+@ExtendWith(MockitoExtension::class)
+class AddressCollectorServiceTest(
+    @Mock
+    private val addressCollectorService: AddressCollectorService
+) {
 
     @DisplayName("도로명주소(한글) 연계정보를 ADSClient 다운로드 요청 실패한다.")
     @Test
     fun receiveFileFailTest() {
         // given
-        val service = mock(AddressCollectorService::class.java)
         val content = ADSContent.JUSUKR
 
         // when
-        assertThrows<Exception> { `when`(service.collect(content)).thenThrow(Exception()) }
+        assertThrows<Exception> { `when`(addressCollectorService.collect(content)).thenThrow(Exception()) }
+    }
+
+    @DisplayName("도로명주소(한글) 연계정보를 다운로드 요청하여 정상 수신하여 건수를 확인한다.")
+    @Test
+    fun receiveFileSuccessTest() {
+        // given
+        val content = ADSContent.JUSUKR
+        val addressContent = listOf(AddressContent(name = "도로명주소(한글)", details = listOf("도로명주소1", "도로명주소2")))
+
+        // when
+        `when`(addressCollectorService.collect(content)).thenReturn(addressContent)
+
+        // then
+        val collection = addressCollectorService.collect(content)[0]
+        assertEquals(collection, addressContent[0])
+        assertEquals(collection.details.size, 2)
+
+        verify(addressCollectorService, times(1)).collect(content)
     }
 
 }
